1cc2e93b5d507e82d057bb572aaec5264f72748e
primo <sim.merdedouch@gmail.com>
2024-04-21 00:34:24 +0100

    I added the Bean class PasswordEncoder

--- src/main/java/net/primodev/gestion_des_patients_app/GestionDesPatientsAppApplication.java
+++ src/main/java/net/primodev/gestion_des_patients_app/GestionDesPatientsAppApplication.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.Date;
 import java.util.List;
 
@@ -37,5 +41,10 @@ public class GestionDesPatientsAppApplication implements CommandLineRunner {
 //        patientRepository.findByIsSick(true, PageRequest.of(0, 4)).getContent().forEach(patient -> {
 //            System.out.println(patient);
 //        });
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
     }
 }
