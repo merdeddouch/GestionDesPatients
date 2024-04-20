066323b104bbb0ef0a028b9a183590959db9b5bd
primo <sim.merdedouch@gmail.com>
2024-04-21 00:36:27 +0100

    I inject the passwordEncoder, and creacted users,thier hashed password and thier roles

--- src/main/java/net/primodev/gestion_des_patients_app/Security/SecurityConfig.java
+++ src/main/java/net/primodev/gestion_des_patients_app/Security/SecurityConfig.java
@@ -1,2 +1,40 @@
-package net.primodev.gestion_des_patients_app.Security;public class SecurityConfig {
+package net.primodev.gestion_des_patients_app.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("TBIB").password(passwordEncoder.encode("AAAA")).roles("USER").build(),
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        return httpSecurity.build();
+    }
+
 }
