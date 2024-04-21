4841b6cd1f876f0f0cd7c209861f74a4e0fb4683
primo <sim.merdedouch@gmail.com>
2024-04-21 10:13:14 +0100

    implement notAuthorizedController

--- src/main/java/net/primodev/gestion_des_patients_app/web/SecurityControler.java
+++
@@ -1,2 +0,0 @@
-package net.primodev.gestion_des_patients_app.web;public class SecurityControler {
-}
---
+++ src/main/java/net/primodev/gestion_des_patients_app/web/SecurityController.java
@@ -0,0 +1,14 @@
+package net.primodev.gestion_des_patients_app.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/notAuthorized")
+    public String notAuthorizedController(){
+        return "notAuthorized";
+    }
+}
