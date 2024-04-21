680d2f5436b8004a43461c99c49b1d7519b79603
primo <sim.merdedouch@gmail.com>
2024-04-21 11:26:32 +0100

    add loginController

--- src/main/java/net/primodev/gestion_des_patients_app/Security/SecurityConfig.java
+++ src/main/java/net/primodev/gestion_des_patients_app/Security/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin();
+        httpSecurity.formLogin().loginPage("/login").permitAll();
+        httpSecurity.authorizeHttpRequests().requestMatchers("/webjars/**").permitAll();
         httpSecurity.authorizeHttpRequests().requestMatchers("/user/**").hasRole("USER");
         httpSecurity.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN");
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
--- src/main/java/net/primodev/gestion_des_patients_app/web/SecurityController.java
+++ src/main/java/net/primodev/gestion_des_patients_app/web/SecurityController.java
@@ -11,4 +11,10 @@ public class SecurityController {
     public String notAuthorizedController(){
         return "notAuthorized";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
 }
